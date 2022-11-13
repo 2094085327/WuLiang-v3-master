3ae217dffac7af667428da8e0718477eff03b035
2094085327 <2094085327@qq.com>
2022-11-13 10:33:33 +0800

    完善天气查询的代码优化，降低代码量

--- wuLiang-v3/src/main/kotlin/pers/wuliang/robot/botApi/geographyApi/GeoApi.kt
+++ wuLiang-v3/src/main/kotlin/pers/wuliang/robot/botApi/geographyApi/GeoApi.kt
@@ -6,43 +6,27 @@ import pers.wuliang.robot.util.HttpUtil
 
 var objectMapper = ObjectMapper()
 
+
 /**
- *@Description:
+ *@Description: 调用天气接口获得数据
  *@Author zeng
  *@Date 2022/11/9 16:51
  *@User 86188
  */
 // 城市信息
 object City {
-        var sendCity: String? = null
-        var code: String? = null
-        var name: String? = null
-        var id: String? = null
-        var lat: String? = null
-        var lon: String? = null
-        var country: String? = null
-        var adm1: String? = null
-        var adm2: String? = null
-        var tz: String? = null
-        var errorMsg: String? = null
+    var sendCity: String? = null
+    var code: String? = null
+    var jsonNode: JsonNode? = null
+    var errorMsg: String? = null
 }
 
 // 城市天气
 object Weather {
-        var code: String? = null
-        var updateTime: String? = null
-        var temp: String? = null
-        var feelsLike: String? = null
-        var text: String? = null
-        var wind360: String? = null
-        var windDir: String? = null
-        var windScale: String? = null
-        var windSpeed: String? = null
-        var humidity: String? = null
-        var pressure: String? = null
-        var vis: String? = null
-        var cloud: String? = null
-        var errorMsg: String? = null
+    var code: String? = null
+    var updateTime: String? = null
+    var jsonNode: JsonNode? = null
+    var errorMsg: String? = null
 }
 
 // 每日提示
@@ -62,24 +46,17 @@ object Prediction {
 }
 
 class GeoApi {
+    private val key = "1b48b4a69c8a4f7cb17674cbf4cea29b"
     fun getCityData(city: String) {
-        val url = "https://geoapi.qweather.com/v2/city/lookup?location=$city&key=1b48b4a69c8a4f7cb17674cbf4cea29b"
+        val url = "https://geoapi.qweather.com/v2/city/lookup?location=$city&key=${key}"
         try {
             City.sendCity = city
             val json = objectMapper.readTree(HttpUtil.get(url).response)
-            if (json["code"].textValue() == "200") {
-                City.code = json["code"].textValue()
-                City.name = json["location"][0]["name"].textValue()
-                City.id = json["location"][0]["id"].textValue()
-                City.lat = json["location"][0]["lat"].textValue()
-                City.lon = json["location"][0]["lon"].textValue()
-                City.country = json["location"][0]["country"].textValue()
-                City.adm1 = json["location"][0]["adm1"].textValue()
-                City.adm2 = json["location"][0]["adm2"].textValue()
-                City.tz = json["location"][0]["tz"].textValue()
+            City.code = json["code"].textValue()
+            if (City.code == "200") {
+                City.jsonNode = json["location"][0]
             } else {
-                City.code = json["code"].textValue()
-                City.errorMsg = "错误码:${City.code}\n阿姬没有找到${city}的地理信息捏\n只能查询城市和省/州的信息哦"
+                City.errorMsg = "阿姬没有找到${city}的地理信息哦\n只能查询城市和省/州的信息"
             }
         } catch (e: Exception) {
             e.printStackTrace()
@@ -89,26 +66,18 @@ class GeoApi {
 
     fun getWeatherData() {
         getDailyWeather()
-        val url = "https://devapi.qweather.com/v7/weather/now?location=${City.id}&key=1b48b4a69c8a4f7cb17674cbf4cea29b"
+        val url = "https://devapi.qweather.com/v7/weather/now?location=${
+            City.jsonNode?.get("id")?.textValue()
+        }&key=${key}"
+        println(url)
         try {
             val json = objectMapper.readTree(HttpUtil.get(url).response)
-            if (json["code"].textValue() == "200") {
-                Weather.code = json["code"].textValue()
+            Weather.code = json["code"].textValue()
+            if (Weather.code == "200") {
                 Weather.updateTime = json["updateTime"].textValue()
-                Weather.temp = json["now"]["temp"].textValue()
-                Weather.feelsLike = json["now"]["feelsLike"].textValue()
-                Weather.text = json["now"]["text"].textValue()
-                Weather.wind360 = json["now"]["wind360"].textValue()
-                Weather.windDir = json["now"]["windDir"].textValue()
-                Weather.windScale = json["now"]["windScale"].textValue()
-                Weather.windSpeed = json["now"]["windSpeed"].textValue()
-                Weather.humidity = json["now"]["humidity"].textValue()
-                Weather.pressure = json["now"]["pressure"].textValue()
-                Weather.vis = json["now"]["vis"].textValue()
-                Weather.cloud = json["now"]["cloud"].textValue()
+                Weather.jsonNode = json["now"]
             } else {
-                Weather.code = json["code"].textValue()
-                Weather.errorMsg = "错误码:${Weather.code}\n阿姬没有找到${City.sendCity}的天气信息捏"
+                Weather.errorMsg = "阿姬没有找到${City.sendCity}的天气信息捏\n换一个试试吧"
             }
         } catch (e: Exception) {
             e.printStackTrace()
@@ -117,8 +86,9 @@ class GeoApi {
     }
 
     private fun getDailyWeather() {
-        val url =
-            "https://devapi.qweather.com/v7/indices/1d?type=1,2&location=${City.id}&key=1b48b4a69c8a4f7cb17674cbf4cea29b"
+        val url = "https://devapi.qweather.com/v7/indices/1d?type=1,2&location=${
+            City.jsonNode?.get("id")?.textValue()
+        }&key=${key}"
         try {
             val json = objectMapper.readTree(HttpUtil.get(url).response)
             if (json["code"].textValue() == "200") {
@@ -136,8 +106,9 @@ class GeoApi {
     }
 
     fun preWeather() {
-        val url =
-            "https://devapi.qweather.com/v7/weather/3d?location=${City.id}&key=1b48b4a69c8a4f7cb17674cbf4cea29b"
+        val url = "https://devapi.qweather.com/v7/weather/3d?location=${
+            City.jsonNode?.get("id")?.textValue()
+        }&key=${key}"
         try {
             val json = objectMapper.readTree(HttpUtil.get(url).response)
             Prediction.code = json["code"].textValue()
@@ -152,4 +123,9 @@ class GeoApi {
             Prediction.errorMsg = "接口肯定是炸了!快来修接口！"
         }
     }
+}
+
+fun main() {
+    GeoApi().getCityData("宁波")
+    GeoApi().getWeatherData()
 }
--- wuLiang-v3/src/main/kotlin/pers/wuliang/robot/botApi/geographyApi/GeoApiUse.kt
+++ wuLiang-v3/src/main/kotlin/pers/wuliang/robot/botApi/geographyApi/GeoApiUse.kt
@@ -3,13 +3,14 @@ package pers.wuliang.robot.botApi.geographyApi
 import love.forte.simboot.annotation.Filter
 import love.forte.simboot.annotation.FilterValue
 import love.forte.simboot.filter.MatchType
+import love.forte.simbot.Api4J
 import love.forte.simbot.event.GroupMessageEvent
 import org.springframework.stereotype.Component
 import pers.wuLiang.robot.core.annotation.RobotListen
 import pers.wuLiang.robot.core.common.send
 
 /**
- *@Description:|\/dl {{city}}
+ *@Description:城市信息查询接口
  *@Author zeng
  *@Date 2022/11/9 18:31
  *@User 86188
@@ -17,18 +18,33 @@ import pers.wuLiang.robot.core.common.send
 @Component
 class GeoApiUse {
 
+    fun getCityData(data: String): String? {
+        return City.jsonNode?.get(data)?.textValue()
+    }
+
+    fun getWeaData(data: String): String? {
+        return Weather.jsonNode?.get(data)?.textValue()
+    }
+
     fun getData(index: Int, data: String): String? {
         return Prediction.jsonNode?.get(index)?.get(data)?.textValue()
     }
 
+    fun clearData(){
+        City.jsonNode=null
+        Weather.jsonNode=null
+        Prediction.jsonNode=null
+    }
+
+    @OptIn(Api4J::class)
     @RobotListen(desc = "查询城市地理情况")
     @Filter("{{city}}地理", matchType = MatchType.REGEX_MATCHES)
     @Filter("/dl {{city}}", matchType = MatchType.REGEX_MATCHES)
     suspend fun GroupMessageEvent.findCity(@FilterValue("city") city: String) {
         // 将城市传回数据接口
         GeoApi().getCityData(city)
-        var cityLon = City.lon?.toFloat()
-        var cityLat = City.lat?.toFloat()
+        var cityLon = getCityData("lon")?.toFloat()
+        var cityLat = getCityData("lat")?.toFloat()
         var cityLonChange: String? = null
         var cityLatChange: String? = null
 
@@ -48,22 +64,21 @@ class GeoApiUse {
             }
         }
         val msg: String = if (City.code == "200") {
-            "城市名称:${City.name}\n" +
-                    "城市ID:${City.id}\n" +
-                    "所属国家:${City.country}\n" +
-                    "上级行政区划:${City.adm1}\n" +
+            "城市名称:${getCityData("name")}\n" +
+                    "城市ID:${getCityData("id")}\n" +
+                    "所属国家:${getCityData("country")}\n" +
+                    "上级行政区划:${getCityData("adm1")}\n" +
                     "城市维度:${cityLatChange}\n" +
                     "城市经度:${cityLonChange}\n" +
-                    "所属时区:${City.tz}"
+                    "所属时区:${getCityData("tz")}"
         } else {
             City.errorMsg.toString()
         }
-        send(msg)
-        City.adm1 = null
-        City.adm2 = null
-        City.id = null
+        replyBlocking(msg)
+        clearData()
     }
 
+    @OptIn(Api4J::class)
     @RobotListen(desc = "查询城市天气情况")
     @Filter("{{city}}天气", matchType = MatchType.REGEX_MATCHES)
     @Filter("/tq {{city}}", matchType = MatchType.REGEX_MATCHES)
@@ -73,15 +88,15 @@ class GeoApiUse {
         GeoApi().getWeatherData()
         GeoApi().preWeather()
         val msg: String = if (Weather.code == "200") {
-            "无量姬·零贰已经获取到了 ${City.adm1 + City.adm2} 的天气呢\n" +
+            "无量姬·零贰已经获取到了 ${getCityData("adm1") + getCityData("adm2")} 的天气呢\n" +
                     "更新时间: \n" +
                     "${Weather.updateTime}\n" +
-                    "气温: ${Weather.temp}°C   体感温度: ${Weather.feelsLike}°C\n" +
-                    "天气状况: ${Weather.text}\n" +
-                    "风力情况: ${Weather.windDir}-${Weather.wind360}°-${Weather.windScale} 级\n" +
-                    "相对湿度: ${Weather.humidity} %\n" +
-                    "大气压强: ${Weather.pressure} 百帕\n" +
-                    "能见度: ${Weather.vis} 公里\n" +
+                    "气温: ${getWeaData("temp")}°C   体感温度: ${getWeaData("feelsLike")}°C\n" +
+                    "天气状况: ${getWeaData("text")}\n" +
+                    "风力情况: ${getWeaData("windDir")}-${getWeaData("wind360")}°-${getWeaData("windScale")} 级\n" +
+                    "相对湿度: ${getWeaData("humidity")} %\n" +
+                    "大气压强: ${getWeaData("pressure")} 百帕\n" +
+                    "能见度: ${getWeaData("vis")} 公里\n" +
                     "洗车指南: ${DailyWeather.carText}\n" +
                     "阿姬小提示: ${DailyWeather.sportText}\n" +
                     "--------${getData(1, "fxDate")}-------\n" +
@@ -91,10 +106,8 @@ class GeoApiUse {
         } else {
             Weather.errorMsg.toString()
         }
-        send(msg)
-        City.adm1 = null
-        City.adm2 = null
-        City.id = null
+        replyBlocking(msg)
+        clearData()
     }
 
     @RobotListen(desc = "城市三天预报")
@@ -110,15 +123,23 @@ class GeoApiUse {
                     "日出时间:${getData(i, "sunrise")}  日落时间:${getData(i, "sunset")}\n" +
                     "最高气温: ${getData(i, "tempMax")}°C  最低气温: ${getData(i, "tempMin")}°C \n" +
                     "日间天气: ${getData(i, "textDay")}  夜间天气: ${getData(i, "textNight")}\n" +
-                    "日间风力:\n${getData(i, "windScaleDay")}级${getData(i, "windDirDay")}-${getData(i, "wind360Day")}° ${getData(i, "windSpeedDay")}m/s\n" +
-                    "夜间风力:\n${getData(i, "windScaleNight")}级${getData(i, "windDirNight")}-${getData(i, "wind360Night")}° ${getData(i, "windSpeedNight")}m/s\n" +
+                    "日间风力:\n${getData(i, "windScaleDay")}级${getData(i, "windDirDay")}-${
+                        getData(
+                            i,
+                            "wind360Day"
+                        )
+                    }° ${getData(i, "windSpeedDay")}m/s\n" +
+                    "夜间风力:\n${getData(i, "windScaleNight")}级${getData(i, "windDirNight")}-${
+                        getData(
+                            i,
+                            "wind360Night"
+                        )
+                    }° ${getData(i, "windSpeedNight")}m/s\n" +
                     "相对湿度: ${getData(i, "humidity")} %\n" +
                     "大气压强: ${getData(i, "pressure")} 百帕\n" +
                     "能见度: ${getData(i, "vis")} 公里"
             send(msg)
         }
-        City.adm1 = null
-        City.adm2 = null
-        City.id = null
+        clearData()
     }
 }
