0c6687275a3274f1efa12f8cd8a96cf48d9031ab
2094085327 <2094085327@qq.com>
2022-12-17 14:56:17 +0800

    迁移2x原神抽卡分析功能，重构代码，大幅度减少代码量，轻微改动算法

---
+++ wuLiang-v3/src/main/kotlin/pers/wuliang/robot/botApi/genShinApi/gaCha/PictureMake.kt
@@ -0,0 +1,223 @@
+package pers.wuliang.robot.botApi.genShinApi.gaCha
+
+import java.awt.*
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.net.URL
+import javax.imageio.ImageIO
+import kotlin.math.ceil
+
+
+/**
+ *@Description: 原神抽卡分析的图片处理类
+ *@Author zeng
+ *@Date 2022/12/16 11:26
+ *@User 86188
+ */
+class PictureMake {
+
+    /**
+     * 成比例缩放
+     * @param imagePath 图片路径
+     * @param newHeight 图片新高度
+     * @param newWidth 图片新宽度
+     */
+    fun resizeProImage(imagePath: String, newWidth: Int, newHeight: Int): BufferedImage? {
+        val imageUrl = URL(GachaConfig.galleryPath + imagePath.urlCode)
+        val image = ImageIO.read(imageUrl)
+        if (image != null) {
+            // 确定新尺寸的比例因子
+            val widthScaleFactor: Double = newWidth.toDouble() / image.width
+            val heightScaleFactor: Double = newHeight.toDouble() / image.height
+            val scaleFactor = Math.min(widthScaleFactor, heightScaleFactor)
+
+            // 计算图像的新大小
+            val scaledWidth = (scaleFactor * image.width).toInt()
+            val scaledHeight = (scaleFactor * image.height).toInt()
+
+            // 使用新大小创建新的空BufferedImage对象
+            val resizedImage = BufferedImage(scaledWidth, scaledHeight, image.type)
+
+            // 缩放原始图像并将其绘制到调整大小的图像上
+            val scaledImage: Image = image.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH)
+            resizedImage.graphics.drawImage(scaledImage, 0, 0, null)
+
+            return resizedImage
+        } else {
+            println("图片获取失败，可能原因为图库中不存在此图片或未配置图库路径")
+            return null
+        }
+    }
+
+    /**
+     * 指定长宽缩放
+     * @param images 图片缓存流
+     * @param newHeight 图片新高度
+     * @param newWidth 图片新宽度
+     */
+    private fun resizeImage(images: BufferedImage, newWidth: Int, newHeight: Int): BufferedImage {
+        val resizedImage = BufferedImage(newWidth, newHeight, images.type)
+        val gd: Graphics2D = resizedImage.createGraphics()
+
+        // 设置图片品质
+        gd.addRenderingHints(RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY))
+        gd.drawImage(images, 0, 0, newWidth, newHeight, null)
+        gd.dispose()
+        // 返回调整大小的图像
+        return resizedImage
+
+    }
+
+    /**
+     * 根据概率判断运气等级
+     * @param probability 概率
+     */
+    private fun choseLuck(probability: Double? = GachaData.probability): BufferedImage {
+        val luck: String = when {
+            probability!! > 80 -> "欧"
+            probability > 60 -> "吉"
+            probability > 40 -> "平"
+            probability > 20 -> "惨"
+            else -> "寄"
+        }
+        return ImageIO.read(URL(GachaConfig.galleryPath + "运气图片/${luck}.png".urlCode))
+    }
+
+    /**
+     *返回每个池子的数据
+     * @param gachaType 池子类型
+     */
+    fun poolImage(gachaType: String): BufferedImage {
+        val white = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/withe.png".urlCode))
+        val string2 = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/${gachaType}文字.png".urlCode))
+        val countWhite = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/计数块.png".urlCode))
+        val luckImage = choseLuck()
+
+        val lines = ceil((GachaMain.dataArray.size / 7.0))
+        val newWhite = resizeImage(white, white.width, (white.height + 506 * lines + 60 * lines).toInt())
+
+        // 创建绘画对象
+        val gd: Graphics2D = newWhite.createGraphics()
+        gd.drawImage(luckImage, 58, 58, luckImage.width, luckImage.height, null)
+
+        gd.drawImage(string2, 1112, 100, string2.width, string2.height, null)
+
+        var roleX = 58
+        var n = 1
+        var counts = 0
+        var lineX = 1
+
+        for (pair in GachaMain.dataArray) {
+            var roleImage = ImageIO.read(URL(GachaConfig.galleryPath + "${gachaType}图片/${pair.key}.png".urlCode))
+            if (roleImage == null) {
+                roleImage = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/default1.png".urlCode))
+            }
+            gd.drawImage(
+                roleImage,
+                roleX + roleImage.width * (lineX - 1),
+                luckImage.height + 106 + (roleImage.height + 50) * (n - 1),
+                roleImage.width,
+                roleImage.height,
+                null
+            )
+
+            gd.drawImage(
+                countWhite,
+                roleX + roleImage.width * (lineX - 1),
+                luckImage.height + 482 + (roleImage.height + 50) * (n - 1),
+                countWhite.width,
+                countWhite.height,
+                null
+            )
+
+            // 设置画笔颜色为黑色，画笔字体样式为微软雅黑，斜体，文字大小为20px
+            gd.color = Color(89, 87, 87)
+            gd.font = Font("汉仪青云简", Font.ITALIC, 100)
+
+            gd.drawString(
+                pair.value.toString(),
+                roleX + 147 + roleImage.width * (lineX - 1),
+                luckImage.height + 575 + (roleImage.height + 50) * (n - 1)
+            )
+
+            roleX += 52
+            counts++
+            lineX++
+            if (counts.toFloat() / 7 == 1f) {
+                n++
+                counts = 0
+                lineX = 1
+                roleX = 58
+            }
+        }
+        gd.color = Color.BLACK
+        gd.font = Font("微软雅黑", Font.ITALIC, 206)
+        gd.drawString(GachaData.aveFive, 1218, 770)
+        gd.drawString(GachaData.count.toString(), 1910, 770)
+        gd.drawString("${GachaData.right}/${GachaMain.dataArray.size}", 2575, 770)
+
+        gd.color = Color(255, 192, 0)
+        gd.font = Font("微软雅黑", Font.ITALIC, 101)
+        gd.drawString(GachaData.haveCost.toString(), 1336, 412)
+        gd.dispose()
+
+        GachaData.count = 0
+        GachaData.haveCost = 0
+        GachaData.right = 0
+
+        return newWhite
+    }
+
+    /**
+     * 生成最终统计数据的图片
+     */
+    fun allDataMake(): BufferedImage {
+        val white = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/withe.png".urlCode))
+        val allData = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/总数据.png".urlCode))
+        val luckImage = choseLuck()
+
+        val gd = white.createGraphics()
+        gd.drawImage(luckImage, 58, 58, luckImage.width, luckImage.height, null)
+        gd.drawImage(allData, 1112, 100, allData.width, allData.height, null)
+
+        gd.color = Color.BLACK
+        gd.font = Font("微软雅黑", Font.ITALIC, 206)
+        gd.drawString(String.format("%.1f", GachaData.finCount / GachaData.finItem.toFloat()), 1190, 770)
+        gd.drawString(GachaData.finCount.toString(), 1864, 770)
+        gd.drawString(GachaData.finItem.toString(), 2591, 770)
+        gd.dispose()
+        return white
+    }
+
+    /**
+     * 合成完整图片
+     * @param totalData 总数据流
+     * @param roleData 角色数据流
+     * @param armsData 武器数据流
+     * @param perData 常驻数据流
+     */
+    fun compositePicture(
+        totalData: BufferedImage,
+        roleData: BufferedImage,
+        armsData: BufferedImage,
+        perData: BufferedImage
+    ): InputStream {
+        val bg = ImageIO.read(URL(GachaConfig.galleryPath + "其他图片/sec.png".urlCode))
+        val newBg =
+            resizeImage(bg, bg.width, 500 + totalData.height + roleData.height + armsData.height + perData.height)
+        val gd = newBg.createGraphics()
+        gd.drawImage(totalData, 100, 100, totalData.width, totalData.height, null)
+        gd.drawImage(roleData, 100, 1242, roleData.width, roleData.height, null)
+        gd.drawImage(armsData, 100, 1342 + roleData.height, armsData.width, armsData.height, null)
+        gd.drawImage(perData, 100, 1442 + roleData.height + armsData.height, perData.width, perData.height, null)
+        gd.dispose()
+
+        // 转换成InputStream输出
+        val byStream = ByteArrayOutputStream()
+        ImageIO.write(newBg, "png", byStream)
+
+        return ByteArrayInputStream(byStream.toByteArray())
+    }
+}
